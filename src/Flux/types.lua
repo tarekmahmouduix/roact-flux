9f7fd545436a15ec94188055f8f47beb9ba2426c
tarekmahmouduix <163179033+tarekmahmouduix@users.noreply.github.com>
2025-07-10 22:32:33 +0300

    Create types.lua

---
+++ src/Flux/types.lua
@@ -0,0 +1,8 @@
+-- @TYPES
+
+export type StyleCard = {
+    n : string,
+    v : string | number | boolean | Color3 | UDim2 | Vector2 | Vector3 | CFrame | EnumItem | Enum | Font | table 
+}
+
+return {}
