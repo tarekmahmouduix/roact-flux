29a77aa4c9882be8fb277cd99f3918feb46af3a6
tarekmahmouduix <163179033+tarekmahmouduix@users.noreply.github.com>
2025-07-10 22:33:00 +0300

    Update types.lua

--- src/Flux/types.lua
+++ src/Flux/types.lua
@@ -2,7 +2,7 @@
 
 export type StyleCard = {
     n : string,
-    v : string | number | boolean | Color3 | UDim2 | Vector2 | Vector3 | CFrame | EnumItem | Enum | Font | table 
+    v : string | number | boolean | Color3 | UDim2 | Vector2 | Vector3 | CFrame | EnumItem | Enum | Font | {} 
 }
 
 return {}
